b21227a54c2e9c3f2a5addc9eb23f81ca5d1dc1b
jasonhsu8 <jace.hsu@gmail.com>
2023-10-19 09:30:43 +0100

    update testsuite

--- subtract_numbers_function.py
+++ subtract_numbers_function.py
@@ -1,2 +1,2 @@
-def subctract_numbers(a, b):
+def subtract_numbers(a, b):
     return a - b
--- test_suite.py
+++ test_suite.py
@@ -1,3 +1,19 @@
-from subtract_numbers_function import subctract_numbers
+import unittest
+from subtract_numbers_function import subtract_numbers
 
-subctract_numbers(8,5)
+class testFunction(unittest.TestCase):
+
+    def test_subtraction(self):
+        result = subtract_numbers(5, 4)
+        self.assertEqual(result, 1)
+    
+    def test_subtraction_negative_numbers(self):
+        result = subtract_numbers(6, -2)
+        self.assertEqual(result, 8)
+
+    def test_subtraction_float_numbers(self):
+        result = subtract_numbers(7.5, 2.5)
+        self.assertAlmostEquals(result, 5.0, places=2 )
+
+if __name__ == '__main__':
+    unittest.main()
